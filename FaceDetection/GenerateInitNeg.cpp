434ed2b0106b5640fa997c4af4821f95ca8ab129
fanglin-ubuntu1404 <hardegg@gmail.com>
2015-07-24 10:33:02 +0800

    Add negative samples generation.

---
+++ FaceDetection/GenerateInitNeg.cpp
@@ -0,0 +1,49 @@
+#include <opencv2/opencv.hpp>
+#include <iostream>
+#include "utilities_common.h"
+
+using namespace std;
+using namespace cv;
+
+int main(int argc, const char* argv[])
+{
+    string folderName = "/home/fanglin/data/VOC2007/nonPerson";
+    string oFolder = "/home/fanglin/data/aflw/aflw/data/neg12x12";
+    vector<string> filePaths;
+    GetFilePaths(folderName, ".jpg", filePaths);
+    vector<Mat> imgs(filePaths.size());
+    for (size_t i = 0; i < filePaths.size(); i++) {
+        imgs[i] = imread(filePaths[i]);
+    }
+    cout << imgs.size() << " images!" << endl;
+    
+    RNG rng( 0xFFFFFFFF );
+    rng.uniform( 0, 100 );
+    int nNegs = 200000;
+    int nDone = 0;
+    while(1) {
+        int idx = rng.uniform(0, imgs.size());
+        int r = rng.uniform(0, imgs[idx].rows);
+        int c = rng.uniform(0, imgs[idx].cols);
+        int s = rng.uniform(0, 2*MIN(MIN(r, imgs[idx].rows-r), MIN(c, imgs[idx].cols-c)) + 1 );
+        
+        if (s < 40)
+            continue;
+        
+        Rect rect = Rect(c-s/2, r-s/2, s, s);
+        Mat patch = imgs[idx](rect);
+        Mat rsz;
+        cv::resize(patch, rsz, cv::Size(12, 12));
+        stringstream ss;
+        ss << oFolder << "/neg_" << nDone << ".bmp";
+        imwrite(ss.str(), rsz);
+        ++nDone;
+        if (nDone == nNegs)
+            break;        
+        if (nDone % 1000 == 0)
+            cout << nDone << " neg generated!" << endl;
+    }
+    
+    
+    return 0;
+}
